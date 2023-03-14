f1063b4070b7408557e199b06f4a1adc4fd56822
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 14:30:47 +0500

    💄 style(About.jsx): add motion animation to introduction paragraph 📝 docs(About.jsx): update introduction paragraph with developer skills and experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,17 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
     </>
   );
 };
