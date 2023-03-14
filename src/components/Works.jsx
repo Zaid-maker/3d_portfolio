ab0c549f93da527a01bfc8be0cdc4404fc092243
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 22:20:10 +0500

    🎨 style(Works.jsx): format code with Prettier 🔥 chore(Works.jsx): remove 'No newline at end of file'

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+  return <div>Works</div>;
+};
 
-export default Works
+export default Works;
