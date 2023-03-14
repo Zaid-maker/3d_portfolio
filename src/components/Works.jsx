a7126cb569c994508c64f905f2d34e843431e64d
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 22:20:45 +0500

    ✨ feat(Works.jsx): add Tilt, motion, styles, github, SectionWrapper, projects, fadeIn, and textVariant imports

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return <div>Works</div>;
