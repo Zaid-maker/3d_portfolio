dce4df96fb56e90e615007c0f4a78ada6b9f57e1
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 17:05:52 +0500

    🎨 chore(Ball.jsx): refactor Ball component to use BallCanvas component ✨ feat(Ball.jsx): add BallCanvas component with 3D rendering capabilities using @react-three/fiber and @react-three/drei libraries 🚚 chore(Ball.jsx): remove unused imports and components

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
-}
+  return <div>Ball</div>;
+};
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return <div>Ball Canvas</div>;
+};
+
+export default BallCanvas;
