e4c212ac7848098a3a0ba6e50bf6e1265cd7466f
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 17:02:25 +0500

    🎨 style(Tech.jsx): add styling to Tech component and render technology icons using BallCanvas component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,15 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  return <div>Tech</div>;
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-20 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SectionWrapper(Tech, "");
