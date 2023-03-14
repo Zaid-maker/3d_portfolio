07c96f7dd65d8402802bc5cd88efe3e9999cedcb
Zaid-maker <pzhafeez@gmail.com>
2023-03-14 16:21:42 +0500

    🎨 style(Experience.jsx): format code with Prettier ✨ feat(Experience.jsx): add VerticalTimeline, VerticalTimelineElement, motion, styles, experiences, SectionWrapper, and textVariant imports 🚀 chore(Experience.jsx): add react-vertical-timeline-component and framer-motion dependencies 🔥 refactor(Experience.jsx): remove unused import statement 👌 refactor(Experience.jsx): add missing semicolon at the end of the line 🔥 chore(Experience.jsx): remove trailing whitespace 🔥 chore(Experience.jsx): remove 'No newline at end of file'

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  return <div>Experience</div>;
+};
 
-export default Experience
+export default Experience;
