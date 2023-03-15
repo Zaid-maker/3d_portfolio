534de8545376e3f915c579aefedeb86ef91cc195
Zaid-maker <pzhafeez@gmail.com>
2023-03-15 17:13:41 +0500

    ✨ feat(Feedbacks.jsx): add framer-motion, styles, SectionWrapper, fadeIn, textVariant, and testimonials constants

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
 
 const Feedbacks = () => {
   return <div>Feedbacks</div>;
