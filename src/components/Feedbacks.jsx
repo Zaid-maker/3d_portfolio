f411dd573a92cb08dfb2f2dee6e661cc6f2406a2
Zaid-maker <pzhafeez@gmail.com>
2023-03-15 17:13:03 +0500

    🎨 style(Feedbacks.jsx): format code with Prettier 🔥 chore(Feedbacks.jsx): remove 'No newline at end of file' message ✨ feat(Feedbacks.jsx): add semicolon to the end of export statement 🚀 perf(Feedbacks.jsx): optimize Feedbacks component by removing unnecessary newline and indentation

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Feedbacks = () => {
-  return (
-    <div>Feedbacks</div>
-  )
-}
+  return <div>Feedbacks</div>;
+};
 
-export default Feedbacks
+export default Feedbacks;
