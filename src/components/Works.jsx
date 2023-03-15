4bde0d7a86e21d9d580cb7904b64afc7cfb53553
Zaid-maker <pzhafeez@gmail.com>
2023-03-15 17:07:14 +0500

    🎨 style(Works.jsx): rearrange imports alphabetically 🔥 chore(Works.jsx): remove unused import statement ✨ feat(Works.jsx): add motion import statement 🚀 perf(Works.jsx): use react-tilt for project card animation 🔥 chore(Works.jsx): remove unused constant 🚀 perf(Works.jsx): use framer-motion for project card animation 🔥 chore(Works.jsx): remove unused import statement 🚀 perf(Works.jsx): use framer-motion for section animation 🚀 perf(Works.jsx): use framer-motion for text animation 🔥 chore(Works.jsx): remove unused import statement 🎨 style(Works.jsx): rearrange import statements alphabetically

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,11 @@
+import { motion } from "framer-motion";
 import React from "react";
 import Tilt from "react-tilt";
-import { motion } from "framer-motion";
 
-import { styles } from "../styles";
 import { github } from "../assets";
-import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
