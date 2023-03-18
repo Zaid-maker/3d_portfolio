6e3c50b4f2aaebe556ab93f730857c5107fc23cb
Zaid-maker <pzhafeez@gmail.com>
2023-03-18 22:32:18 +0500

    🎨 style(Contact.jsx): add motion animation to Contact component 🚀 perf(Contact.jsx): optimize Contact component by using motion animation instead of static rendering

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,26 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-0 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
 };
 
 export default SectionWrapper(Contact, "contact");
