b8ea05af957bfdb2bbe31f7009f26ce04a4c5cbd
Zaid-maker <pzhafeez@gmail.com>
2023-03-19 22:40:39 +0500

    🎨 style(Earth.jsx): format code and add 3D model of Earth to the canvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
