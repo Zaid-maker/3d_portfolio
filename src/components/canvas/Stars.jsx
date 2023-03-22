1a88005446afff92f3d1e4874ff7b47f61c07dc5
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 20:25:43 +0500

    ✨ feat(Stars.jsx): add StarsCanvas component to render stars on canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -7,4 +7,8 @@ const Stars = () => {
   return <div>Stars</div>;
 };
 
-export default Stars;
+const StarsCanvas = () => {
+  return <div className="w-full h-auto absolute inset-0 z-[-1]"></div>;
+};
+
+export default StarsCanvas;
