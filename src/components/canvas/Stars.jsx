b7c20a12b440db036d7f7108ed78d5d267af311e
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 20:37:58 +0500

    🎨 style(Stars.jsx): format code with prettier ✨ feat(Stars.jsx): add rotating animation to the stars group 🐛 fix(Stars.jsx): fix incorrect import statement for random library 🔥 chore(Stars.jsx): remove unused component and import statements 🚀 perf(Stars.jsx): optimize performance by setting frustumCulled prop to true on Points component 🎨 style(Stars.jsx): add whitespace for readability

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,8 +3,28 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
-  return <div>Stars</div>;
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000)));
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
 const StarsCanvas = () => {
