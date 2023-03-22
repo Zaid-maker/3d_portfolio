f4d1c22e78657e26a162b0dfac80955c62c5835e
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 20:31:50 +0500

    🎨 style(Stars.jsx): format code and add missing whitespace ✨ feat(Stars.jsx): add Three.js Canvas component with Stars and Preload components inside

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,7 +8,17 @@ const Stars = () => {
 };
 
 const StarsCanvas = () => {
-  return <div className="w-full h-auto absolute inset-0 z-[-1]"></div>;
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
 };
 
 export default StarsCanvas;
