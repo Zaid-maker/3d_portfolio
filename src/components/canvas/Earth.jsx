cd8ec2ffc23f72f9ff20ec0e4967488a056dc32a
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 19:50:19 +0500

    🎨 style(Earth.jsx): reorder imports alphabetically and group them by package name

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
 
 import CanvasLoader from "../Loader";
 
