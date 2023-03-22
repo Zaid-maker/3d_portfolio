eb77171298b304eec4592d6a291186c2b8c4cb33
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 20:10:27 +0500

    🔥 refactor(Contact.jsx): remove unused form_name property from email object

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,6 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          form_name: form.name,
           from_name: form.name,
           to_name: "JavaScript Mastery",
           from_email: form.email,
