236ee33a93c477467d2ba7c296e16adb98a28925
Zaid-maker <pzhafeez@gmail.com>
2023-03-22 20:39:18 +0500

    🐛 fix(Stars.jsx): add stride prop to Points component to fix rendering issue ✨ feat(Stars.jsx): add radius option to inSphere function to increase sphere size

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,9 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000)));
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -14,7 +16,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
