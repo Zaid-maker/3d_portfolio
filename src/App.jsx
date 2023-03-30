6615a23650051f3b3f2ea313bfd1b24bf5fb74e6
Zaid-maker <pzhafeez@gmail.com>
2023-03-31 00:37:03 +0500

    🐛 fix(App.jsx): add missing space in className attribute of div element

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeatbg bg-center">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
